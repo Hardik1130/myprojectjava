5537b7d52f0b2cff90eccadc97c281bb19b58b0e
hardik <hardikgupta176@gmail.com>
2022-09-24 14:28:30 +0530

    my First Git

---
+++ hardik11/src/hardik11/newwww.java
@@ -0,0 +1,27 @@
+package hardik11;
+
+import java.util.Scanner;
+
+public class newwww {
+
+	public static void main(String[] args) {
+		{
+
+	        Scanner in = new Scanner(System.in);
+	        System.out.println("Enter the first number:");
+	        int i = in.nextInt();
+
+	        System.out.println("Enter the Second number:");
+	        int j = in.nextInt();
+
+	        //code for swapping
+	        i = i+j;
+	        j = i-j;
+	        i = i-j;
+
+	        System.out.println("After swapping the numbers:"+i+","+j);
+	    }
+
+	}
+
+}
--- hardikproject/src/hardikproject1/swap1.java
+++ hardikproject/src/hardikproject1/swap1.java
@@ -1,5 +1,23 @@
 package hardikproject1;
 
-public class swap1 {
+import java.util.Scanner;
 
+public class swap1 {
+	public static void main(String[] args)
+	{
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the first number:");
+		int i = in.nextInt();
+		
+		System.out.println("Enter the Second number:");
+		int j = in.nextInt();
+		
+		//code for swapping
+		i = i+j;
+		j = i-j;
+		i = i-j;
+		
+		System.out.println("After swapping the numbers:"+i+","+j);
+	}
 }
